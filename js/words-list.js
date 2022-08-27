31eb174a8309d07f756b2602311a98e201940539
Giselle Lacuesta Leal <105171250+GiselleLacuesta@users.noreply.github.com>
2022-08-27 15:55:57 -0300

    Add files via upload

---
+++ js/words-list.js
@@ -0,0 +1,17 @@
+let wordsList = [
+    "GALAXIA",
+    "ASTEROIDE",
+    "METEORITO", 
+    "ESTRELLA",
+    "LUNA",
+    "MERCURIO",
+    "VENUS", 
+    "TIERRA", 
+    "MARTE", 
+    "JUPITER",
+    "SATURNO",
+    "NEPTUNO",
+    "URANO" ,
+    "PLUTON",
+    "CONSTELACION"
+];
